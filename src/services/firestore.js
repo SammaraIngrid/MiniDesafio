ad3240149cbff6c48d92efc1ebe934239e4ea21f
Sammara Ingrid <sammaraingrid@Sammaras-MacBook-Air.local>
2025-11-26 19:56:02 -0300

    fix firestore

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,11 +1,6 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
 
-export async function getChallenges() {
-  const snapshot = await getDocs(collection(db, "challenges"));
-  return snapshot.docs.map((doc) => doc.data());
-}
-
 export async function getMotivations() {
   const snapshot = await getDocs(collection(db, "motivations"));
   return snapshot.docs.map((doc) => doc.data());
